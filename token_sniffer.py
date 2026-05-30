7e562cc5b94b41c545a56ef6ffb2c275560994b8
Rizal Codes <rizalical231@gmail.com>
2026-05-30 17:16:23 +0800

    Update token_sniffer.py

--- token_sniffer.py
+++ token_sniffer.py
@@ -27,10 +27,10 @@ log = logging.getLogger(__name__)
 # ─────────────────────────────────────────────
 # CONFIG
 # ─────────────────────────────────────────────
-ETHERSCAN_API_KEY = os.getenv("ETHERSCAN_API_KEY", "AW8AJ3TQV79VTM1WM9KY7W9H5ICZZ1WUYT")
-INFURA_URL        = os.getenv("INFURA_URL",        "https://mainnet.infura.io/v3/e1576449bd6142eba99fd3cc4f3fe7b3")
-TELEGRAM_TOKEN    = os.getenv("TELEGRAM_TOKEN",    "8660442841:AAE1oCT6WkyhVdE9eC46I-YOD-FNBjeomYY")
-TELEGRAM_CHAT_ID  = os.getenv("TELEGRAM_CHAT_ID",  "1024188205")
+ETHERSCAN_API_KEY = os.getenv("ETHERSCAN_API_KEY", "Your_Etherscan_Api_Here")
+INFURA_URL        = os.getenv("INFURA_URL",        "https://mainnet.infura.io/v3/Your_Infure_Key_Here")
+TELEGRAM_TOKEN    = os.getenv("TELEGRAM_TOKEN",    "Your_Bot_Token_Here")
+TELEGRAM_CHAT_ID  = os.getenv("TELEGRAM_CHAT_ID",  "Your_Chat_ID_Here")
 
 # Safety thresholds
 MIN_LIQUIDITY_USD    = 10000   # minimum liquidity untuk dianggap legitimate ($10k)
